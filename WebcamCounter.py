086fd31a953cc60f622b09c2c55488370e7439d2
Michael Adams <github@mmmad.de>
2020-05-18 12:12:45 +0100

    Typo

--- WebcamCounter.py
+++ WebcamCounter.py
@@ -157,7 +157,7 @@ if __name__ == '__main__':
                {'ID':60,'URL':'http://www.webcam-rimbach.de/images/c1image_big.jpg', 'Lat':'49.623281',"Lon":'8.7570742','Name':'Rimbach Marktplatz', 'Personenzahl':None, 'Stand':None },
                {'ID':61,'URL':'https://www.hal-oever.de/webcam/schlastshut.jpg', 'Lat':'53.0749188',"Lon":'8.8016672','Name':'Bremen Werserpromenande', 'Personenzahl':None, 'Stand':None },
                {'ID':62,'URL':'http://fischmarkt.no-ip.biz/record/current.jpg', 'Lat':'53.5440922',"Lon":'9.9187187','Name':'Hamburg Fischmarkt', 'Personenzahl':None, 'Stand':None },               
-               {'ID':63,'URL':'https://www.vvo-online.de/img/webcamimages/marienbruecke-102.jpg', 'Lat':'51.0615957,',"Lon":'13.7320668','Name':'Dresden Marienbrücke', 'Personenzahl':None, 'Stand':None },
+               {'ID':63,'URL':'https://www.vvo-online.de/img/webcamimages/marienbruecke-102.jpg', 'Lat':'51.0615957',"Lon":'13.7320668','Name':'Dresden Marienbrücke', 'Personenzahl':None, 'Stand':None },
                {'ID':64,'URL':'https://www.vvo-online.de/img/webcamimages/koenigsbruecker_strasse_richtung_sued-111.jpg', 'Lat':'51.0700056',"Lon":'13.74682','Name':'Dresden Königsbrücker Str.', 'Personenzahl':None, 'Stand':None },  
                {'ID':65,'URL':'https://www.vvo-online.de/img/webcamimages/albertbruecke-135.jpg', 'Lat':'51.0571946',"Lon":'13.7523692','Name':'Dresden Albertbrücke', 'Personenzahl':None, 'Stand':None },  
                {'ID':66,'URL':'https://www.vvo-online.de/img/webcamimages/bergstrasse-149.jpg', 'Lat':'51.0302823',"Lon":'13.7284321','Name':'Dresden Bergstraße', 'Personenzahl':None, 'Stand':None }, 
